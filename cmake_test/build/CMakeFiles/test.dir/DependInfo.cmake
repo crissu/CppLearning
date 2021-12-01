256aa66db03d551686e622112faf600384ddaae6
crissu <13750782397@163.com>
2021-12-01 07:56:24 +0000

    feat:use cmake

---
+++ cmake_test/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/crissu/codeplace/cpp_projects/CppLearning/cmake_test/src/main.cpp" "/home/crissu/codeplace/cpp_projects/CppLearning/cmake_test/build/CMakeFiles/test.dir/src/main.cpp.o"
+  "/home/crissu/codeplace/cpp_projects/CppLearning/cmake_test/src/test.cpp" "/home/crissu/codeplace/cpp_projects/CppLearning/cmake_test/build/CMakeFiles/test.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_test/src/main.cpp
@@ -0,0 +1,9 @@
+//main.cpp
+#include <iostream>
+#include"test.h"
+
+int main(int argc, char** argv)
+{
+    myprint();
+    return 0;
+}
